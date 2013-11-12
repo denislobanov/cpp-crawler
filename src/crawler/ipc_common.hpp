fe19d21b006e9dc8b66c92d1345f2467d8dfcfac
Denis I Lobanov <lobanov.himself@gmail.com>
2013-11-12 21:47:20 +0000

    ipc protocol defined
    ipc base class needed
        * client and server specialise
        * both client and server use two ports
            -- listen on one for commands, reply on the other
            -- each has one acceptor port
            -- outbound on others acceptor
        * both perform async requests and service them
            -- master requests async status updates
            -- worker async data, config/registration requests

--- src/crawler/ipc_common.hpp
+++ src/crawler/ipc_common.hpp
@@ -3,9 +3,10 @@
 
 #include <glibmm/ustring.h>
 
-#define CNC_SERVICE_NAME "worker_cnc"
-#define TO_WORKER_SERVICE_NAME "work_in"
-#define FROM_WORKER_SERVICE_NAME "work_out"
+#include "page_data.hpp"
+
+#define MASTER_SERVICE_NAME "master_in"
+#define WORKER_SERVICE_NAME "worker_in"
 
 /**
  * status of crawler_worker
@@ -22,24 +23,18 @@ enum worker_status {
 /**
  * CnC intructions to workers
  */
-enum worker_intruction {
-    START,          //begin processing data from work queue
-    STOP,           //finish current processing and idle
-    KILL,           //die now
-    STATUS,         //report current status
-    NO_INST         //no instruction
-};
-
-enum master_instruction {
-    BOOTSTRAP,      //provide initial configuration
-    GET_WORK,       //request work items
-    NO_INST         //no request, checking in
+enum cnc_instruction {
+    w_register,     //worker requests config
+    w_get_work,     //worker requests work
+    m_config,       //master sent config data
+    m_send_status,  //master requests worker status
+    m_url_data      //master sent work
 };
 
 /**
- * configuration sent to worker
+ * worker config/registration data
  */
-enum tag_type_e {
+enum tag_type_e {            //part of parser configuration
     invalid,
     url,
     title,
@@ -49,16 +44,18 @@ enum tag_type_e {
     image
 };
 
-struct tagdb_s {
+struct tagdb_s {            //part of parser configuration
     tag_type_e tag_type;    //meta
 
     Glib::ustring xpath;    //xpath to match node
     Glib::ustring attr;     //needed to extract attr_data from libxml2
 };
 
+
 struct worker_config {
-    std::string user_agent;
-    unsigned int day_max_crawls;
+    std::string user_agent;         //to report to sites
+    unsigned int day_max_crawls;    //per page
+    unsigned int worker_id;
 
     //cache config
     unsigned int page_cache_max;
@@ -67,7 +64,7 @@ struct worker_config {
     unsigned int robots_cache_res;
 
     //database
-    std::string db_path;
+    std::string db_path;            //uri
 
     //parser
     std::vector<struct tagdb_s> parse_param;
@@ -77,8 +74,27 @@ struct worker_config {
  * capabilities reported by worker
  */
 struct capabilities {
-    unsigned int parsers;
+    unsigned int parsers;           //parser threads
     unsigned int total_threads;
+};
+
+/**
+ * Communication structure
+ *  - homologates sending/recieving of CnC & URL data as well as intstructions
+ */
+struct ipc_message {
+    enum message_type {
+        instruction,    //cnc_instruction
+        cnc_data,       //worker_config or capabilities depending on previous cnc_instruction
+        queue_node      //queue_node_s
+    };
+
+    union data {
+        cnc_instruction instruction;
+        struct worker_config config;
+        struct capabilities cap;
+        struct queue_node_s node;
+    };
 };
 
 #endif
